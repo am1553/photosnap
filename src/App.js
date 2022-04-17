2451ce52ccce1a78ed75a1fb42c14db036685b76
Aryan Personal <aryanpersonal@Aryans-Air.cust.communityfibre.co.uk>
2022-04-17 01:26:00 +0100

    completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./routes/Home/Home";
@@ -11,10 +11,10 @@ function App() {
 
   return (
     <div className="App">
-        <Router>
+        <Router hashType='hashbang'>
           <Header />
           <Routes>
-            <Route path='/' element={ <Home /> }/>
+            <Route path='/' exact element={ <Home /> }/>
             <Route path='/stories' element={ <Stories /> }/>
             <Route path='/features' element={ <Features /> }/>
             <Route path='/pricing' element={ <Pricing /> }/>
